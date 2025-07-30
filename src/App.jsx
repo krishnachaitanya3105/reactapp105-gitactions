6f1e710cd89bd8c63bf8927f7d92a42fa06af911
krishnachaitanya3105 <krishnachaitanya31052006@gmail.com>
2025-07-30 10:43:32 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
+      <h2>CI/CD Cloud Devops Course</h2>
+
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
