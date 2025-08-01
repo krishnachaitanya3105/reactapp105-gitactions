6ba76d84aa9fb9c97c306e3c25d0e181744a27f0
krishnachaitanya3105 <krishnachaitanya31052006@gmail.com>
2025-08-01 18:21:08 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './style.css'
+import './index.css';
 
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome to My React Project - S105</h1>
-      <h2>CI/CD Cloud Devops Course</h2>
+    <>
+      <nav className="navbar">
+        <div className="logo">S105</div>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Course</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
 
-      <p>This is a simple homepage deployed on GitHub Actions Pages using Vite.</p>
-      <h1>Done with Git Pages & Git Actions</h1>
-    </div>
+      <div className="container">
+        <h1>Welcome to My React Project - S105</h1>
+        <h2>CI/CD Cloud DevOps Course</h2>
+        <p>This is a simple homepage deployed on GitHub Actions Pages using Vite.</p>
+        <h1>Done with Git Pages & Git Actions</h1>
+      </div>
+    </>
   );
 }
 
